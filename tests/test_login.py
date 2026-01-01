d1d0420334175061cc94ce40939e3026e0b38042
ShripadKale <kaleshripad11@gmail.com>
2026-01-01 19:53:52 +0530

    Updated login test

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,50 +4,51 @@ from playwright.sync_api import Page
 from utils import config_reader
 from utils import log_manager
 import inspect
+import pytest
 
 configs = config_reader.read_config()
 logger = log_manager.get_logger("TestLogin")
 
+@pytest.fixture
+def login(page):
+    page.goto(configs["base_url"])
+    return PageLogin(page)
+
+@pytest.fixture
+def dashboard(page):
+    return PageDashboard(page)
+
 class TestLogin:
-    def test_login_with_valid_credentials(self, page: Page):
+    def test_login_with_valid_credentials(self,login, dashboard):
         logger.info(f"Starting test: {inspect.currentframe().f_code.co_name}")
         """Verify, user logs in successfully with valid credentials"""
-        page.goto(configs["base_url"])
-        login = PageLogin(page)
-        dashboard = PageDashboard(page)
         login.enter_username(configs["username"])
         login.enter_password(configs["password"])
         login.click_login_button()
         dashboard.is_header_displayed()
         logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Test Finished~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-    def test_login_with_invalid_user_valid_password(self, page: Page):
+    def test_login_with_invalid_user_valid_password(self,login):
         logger.info(f"Starting test: {inspect.currentframe().f_code.co_name}")
         """Verify, login fails when user enters invalid username"""
-        page.goto(configs["base_url"])
-        login = PageLogin(page)
         login.enter_username("admins")
         login.enter_password(configs["password"])
         login.click_login_button()
         assert login.get_alert_message() == "Invalid credentials", f"Expected 'Invalid credentials, got '{login.get_alert_message()}'"
         logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Test Finished~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-    def test_login_with_valid_user_invalid_password(self, page: Page):
+    def test_login_with_valid_user_invalid_password(self,login):
         logger.info(f"Starting test: {inspect.currentframe().f_code.co_name}")
         """Verify, login fails when user enters valid username & invalid password"""
-        page.goto(configs["base_url"])
-        login = PageLogin(page)
         login.enter_username(configs["username"])
         login.enter_password("pass")
         login.click_login_button()
         assert login.get_alert_message() == "Invalid credentials", f"Expected 'Invalid credentials, got '{login.get_alert_message()}'"
         logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Test Finished~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-    def test_login_with_invalid_user_and_invalid_password(self, page: Page):
+    def test_login_with_invalid_user_and_invalid_password(self,login):
         logger.info(f"Starting test: {inspect.currentframe().f_code.co_name}")
         """Verify, login fails when user enters invalid username & invalid password"""
-        page.goto(configs["base_url"])
-        login = PageLogin(page)
         login.enter_username("userx")
         login.enter_password("pass")
         login.click_login_button()
